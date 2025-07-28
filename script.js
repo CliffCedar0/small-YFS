63331b60b239cf806fc9557a6e1c6f6c907b13ec
CliffCedar0 <95428821+smallfats0@users.noreply.github.com>
2025-07-28 20:48:41 +0800

    优化

--- script.js
+++ script.js
@@ -4045,7 +4045,7 @@ function showSancengpan(tianpanMap) {
     <div class="row mt-4" id="sicengpan-btn-container">
         <div class="col-md-12 mb-3">
             <div class="card">
-                <div class="card-header bg-dark text-white">
+                <div class="card-header bg-primary text-white">
                     第四层盘
                 </div>
                 <div class="card-body text-center">
